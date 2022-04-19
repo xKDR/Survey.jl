943808d34e16ab5671a9b0aa14c55056f61ee60f
ayushpatnaikgit <ayushpatnaik@gmail.com>
2022-04-19 23:44:34 +0530

    Adding Base.Show for svydesign and other syntax modifications

--- src/Survey.jl
+++ src/Survey.jl
@@ -1,26 +1,61 @@
 module Survey
 
+using DataFrames
+using Statistics
+
+export svydesign
+export svyby
+
 struct svydesign
     ids::Symbol
-    probs
+    probs::Symbol
     strata::Symbol
-    variables
-    fpc
-    data
+    variables::Symbol
+    fpc::Symbol
+    data::DataFrame
     nest::Bool
-    check_strat
-    weights::Symbol
-    svydesign(; ids = nothing, probs = nothing, strata = nothing, variables = nothing, fpc = nothing, data = nothing, nest = nothing, check_strat = nothing, weights = nothing) = new(ids, probs, strata, variables, fpc, data, nest, check_strat, weights)
+    check_strat::Bool
+    weights::Symbol 
+    svydesign(; data = DataFrame(), ids = Symbol(), probs = Symbol(), strata = Symbol(), variables = Symbol(), fpc = Symbol(), nest = false, check_strat = false, weights = Symbol()) = new(ids, probs, strata, variables, fpc, data, nest, check_strat, weights)
 end
 
-
-function svyby(formula, by, design::svydesign, func, params)
+function svyby(formula, by, design::svydesign, func, params = [])
     gdf = groupby(design.data, by)
-    weights = design.weights
-    statistic = combine(gdf, [formula, weights] => ((formula, weights) -> func(formula, weights(weights)), params...) => :statistic)
+    w = design.weights
+    statistic = combine(gdf, [formula, w] => ((x, y) -> func(x, weights(y), params...)) => :stats)
     return statistic
 end
 
+function Base.show(io::IO, design::svydesign)
+    printstyled("Survey Design:\n") 
+    printstyled("data: "; bold = true)
+    print(size(design.data)[1], "x", size(design.data)[2], " DataFrame")
+    if length(string(design.weights)) > 0 
+        printstyled("\nweights: "; bold = true)
+        print(design.weights)
+    end
+    if length(string(design.ids)) > 0
+        printstyled("\nid: "; bold = true)
+        print(design.ids)
+    end
+    if length(string(design.strata)) > 0
+        printstyled("\nid: "; bold = true)
+        print(design.strata)
+    end
+    if length(string(design.variables)) > 0
+        printstyled("\nid: "; bold = true)
+        print(design.variables)
+    end
+    if length(string(design.fpc)) > 0
+        printstyled("\nid: "; bold = true)
+        print(design.fpc)
+    end
+    printstyled("\nnest: "; bold = true)
+    print(design.nest)
+    printstyled("\ncheck_strat: "; bold = true)
+    print(design.check_strat)
+end
 # unstack(mean_income, :HR, :statistic)
 
 end
+
