c8cfa7a2c78cae8d7f2adf8a20142854226572d5
ayushpatnaikgit <ayushpatnaik@gmail.com>
2022-04-19 19:28:14 +0530

    Had to declare weights = design.weights for combine to work.

--- src/Survey.jl
+++ src/Survey.jl
@@ -1,22 +1,23 @@
 module Survey
 
 struct svydesign
-    ids
+    ids::Symbol
     probs
-    strata
+    strata::Symbol
     variables
     fpc
     data
-    nest
+    nest::Bool
     check_strat
-    weights
+    weights::Symbol
+    svydesign(; ids = nothing, probs = nothing, strata = nothing, variables = nothing, fpc = nothing, data = nothing, nest = nothing, check_strat = nothing, weights = nothing) = new(ids, probs, strata, variables, fpc, data, nest, check_strat, weights)
 end
 
-svydesign(; ids = nothing, probs = nothing, strata = nothing, variables = nothing, fpc = nothing, data = nothing, nest = nothing, check_strat = nothing, weights = nothing) = svydesign(ids, probs, strata, variables, fpc, data, nest, check_strat, weights)
 
 function svyby(formula, by, design::svydesign, func, params)
     gdf = groupby(design.data, by)
-    statistic = combine(gdf, [formula, design.weights] => ((formula, design.weights) -> func(formula, weights(design.weights)), params...) => :statistic)
+    weights = design.weights
+    statistic = combine(gdf, [formula, weights] => ((formula, weights) -> func(formula, weights(weights)), params...) => :statistic)
     return statistic
 end
 
