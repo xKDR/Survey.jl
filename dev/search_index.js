5415e0622b44eab1460383f4ef543b4dcaa87fdf
Documenter.jl <documenter@juliadocs.github.io>
2022-04-19 12:46:17 +0000

    build based on c4ae83e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Survey","category":"page"},{"location":"#Survey","page":"Home","title":"Survey","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Survey.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Survey]","category":"page"}]
+}
