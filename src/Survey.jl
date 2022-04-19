eeec3bb34cba7e394e069142a92a322ade1fc8f4
ayushpatnaikgit <ayushpatnaik@gmail.com>
2022-04-19 19:05:22 +0530

    Adding svydesign and svyby functions

--- src/Survey.jl
+++ src/Survey.jl
@@ -1,5 +1,25 @@
 module Survey
 
-# Write your package code here.
+struct svydesign
+    ids
+    probs
+    strata
+    variables
+    fpc
+    data
+    nest
+    check_strat
+    weights
+end
+
+svydesign(; ids = nothing, probs = nothing, strata = nothing, variables = nothing, fpc = nothing, data = nothing, nest = nothing, check_strat = nothing, weights = nothing) = svydesign(ids, probs, strata, variables, fpc, data, nest, check_strat, weights)
+
+function svyby(formula, by, design::svydesign, func, params)
+    gdf = groupby(design.data, by)
+    statistic = combine(gdf, [formula, design.weights] => ((formula, design.weights) -> func(formula, weights(design.weights)), params...) => :statistic)
+    return statistic
+end
+
+# unstack(mean_income, :HR, :statistic)
 
 end
