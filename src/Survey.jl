ee125deab210cc94e1d8422a38ab7959c0685dca
harsharora21 <harsharora1337@gmail.com>
2022-06-11 17:26:07 +0530

    fixed svyglm

--- src/Survey.jl
+++ src/Survey.jl
@@ -4,12 +4,14 @@ using DataFrames
 using Statistics
 using StatsBase
 using CSV
+using GLM
 
 include("svydesign.jl")
 include("svyby.jl")
 include("example.jl")
+include("svyglm.jl")
 
-export svydesign, svyby
+export svydesign, svyby, svyglm
 export data, api, apiclus1, apiclus2, apipop, apistrat, apisrs
 export svymean, svytotal, svyquantile
 
--- src/svyglm.jl
+++ src/svyglm.jl
@@ -1,3 +1,5 @@
 function svyglm(formula,design,dist,link)
-    glm(formula,design.data,dist,link,wts=(1.0./design.prob),rtol=1e-8,atol=1e-8)
+    data = design.data
+    weights = (1.0./data[:,design.probs])
+    glm(formula,data,dist,link,wts=weights,rtol=1e-8,atol=1e-8)
 end
