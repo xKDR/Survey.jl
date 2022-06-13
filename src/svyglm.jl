de87ca9343a1b3d6a7a0a54b3ff4d4afb7b54215
harsharora21 <harsharora1337@gmail.com>
2022-06-13 15:09:35 +0530

    created svyglm struct

--- src/svyglm.jl
+++ src/svyglm.jl
@@ -1,5 +1,20 @@
+using GLM
+
+struct svyglm_obj
+    glm::Any
+    coefficients::Any
+    data::Any
+    weights::Any
+    svyglm_obj(glm, data, weights) = new(glm, coef(glm), data, weights)
+end
+
 function svyglm(formula,design,dist,link)
     data = design.data
-    weights = (1.0./data[:,design.probs])
-    glm(formula,data,dist,link,wts=weights,rtol=1e-8,atol=1e-8)
+    if design.probs != Symbol("")
+        weights = (1.0./data[:,design.probs])
+    else
+        weights = ones(size(data)[1])
+    end
+    absglm = glm(formula,data,dist,link,wts=weights,rtol=1e-8,atol=1e-8)
+    svyglm_obj(absglm,data,weights)
 end
