b2b40affc7b27e4012d4c6131fda2c34eec71e83
harsharora21 <harsharora1337@gmail.com>
2022-06-20 17:33:30 +0530

    exported GLM distributions and link functions

--- src/Survey.jl
+++ src/Survey.jl
@@ -15,6 +15,21 @@ include("svyglm.jl")
 export svydesign, svyby, svyglm
 export data, api, apiclus1, apiclus2, apipop, apistrat, apisrs
 export svymean, svytotal, svyquantile
-
+export @formula
+export
+    #families
+    Normal,
+    Binomial,
+    Gamma,
+    Poisson,
+    #links
+    IdentityLink,
+    InverseLink,
+    LogLink,
+    LogitLink,
+    ProbitLink,
+    CauchitLink,
+    CloglogLink,
+    SqrtLink
 end
 
