5197aca567cdfe424275a13d7c1616d9fad6dd79
ayushpatnaikgit <ayushpatnaik@gmail.com>
2022-07-10 17:52:03 +0530

    Changing survey design. Adding SE for mean

--- src/svyby.jl
+++ src/svyby.jl
@@ -1,37 +1,31 @@
-svymean = mean
-svyquantile = quantile
-svysum = wsum
 
-"""
-```julia
-svyby(formula::Symbol, design::svydesign, func::Function, params = [])
-```
+function svymean(x, w, popsize, sampsize)
+    if !(nothing in [popsize])
+        ps = maximum(popsize)
+    end
+    ss = maximum(sampsize)
+    function SE(x, w, ss)
+        f = sqrt(1 - 1/length(x) + 1/ss)
+        var = sum(w.*(x.- sum(w.*x)/sum(w)).^2)/sum(w)
+        sd = sqrt(var / (length(x) -1))
+        return f * sd
+    end
+   return DataFrame(mean = mean(x, weights(w)), SE = SE(x, w, ss))
+end
 
-The `svyby` function can be used to generate stratified estimates.
+svyquantile = function(x, w, popsize, sampsize, q)
+    df = DataFrame(tmp = quantile(Float32.(x), weights(w), q))
+    rename!(df,:tmp => Symbol(string(q) .* "th percentile"))
+    return df
+end
 
-```jldoctest
-julia> using Survey  
 
-julia> data(api); 
-
-julia> dclus1 = svydesign(id=:dnum, weights=:pw, data = apiclus1, fpc=:fpc);
-
-julia> svyby(:api00, dclus1, svymean)
-644.1693989071047
-```
-"""
-function svyby(formula::Symbol, design::svydesign, func::Function, params = [])
-    data = design.data
-    x = design.data[!, string(formula)]
-    w = design.data[!, string(design.weights)]
-    return func(x, weights(w), params...)
+svytotal = function(x, w, popsize, sampsize)
+    df = DataFrame(total = wsum(Float32.(x), weights(1 ./ w)))
+    return df
 end
 
 """
-```julia
-svyby(formula::Symbol, by::Symbol, design::svydesign, func::Function, params = [])
-```
-
 The `svyby` function can be used to generate stratified estimates.
 
 ```jldoctest
@@ -42,49 +36,26 @@ julia> data(api);
 julia> dclus1 = svydesign(id=:dnum, weights=:pw, data = apiclus1, fpc=:fpc); 
 
 julia> svyby(:api00, :cname, dclus1, svymean)
-11×2 DataFrame
- Row │ cname        api00   
-     │ String15     Float64 
-─────┼──────────────────────
-   1 │ Alameda      669.0
-   2 │ Fresno       472.0
-   3 │ Kern         452.5
-   4 │ Los Angeles  647.267
-   5 │ Mendocino    623.25
-   6 │ Merced       519.25
-   7 │ Orange       710.563
-   8 │ Plumas       709.556
-   9 │ San Diego    659.436
-  10 │ San Joaquin  551.189
-  11 │ Santa Clara  732.077
-```
-"""
-function svyby(formula::Symbol, by::Symbol, design::svydesign, func::Function, params = [])
-    gdf = groupby(design.data, by)
-    w = design.weights
-    return combine(gdf, [formula, w] => ((x, y) -> func(x, weights(y), params...)) => formula)
-end
-
-
-"""
-```julia
-svyby(formula::Symbol, by::Vector{Symbol}, design::svydesign, func::Function, params = [])
+11×3 DataFrame
+ Row │ cname        mean     SE
+     │ String15     Float64  Float64
+─────┼────────────────────────────────
+   1 │ Alameda      669.0    16.2135
+   2 │ Fresno       472.0     9.85278
+   3 │ Kern         452.5    29.5049
+   4 │ Los Angeles  647.267  23.5116
+   5 │ Mendocino    623.25   24.216
+   6 │ Merced       519.25   10.4925
+   7 │ Orange       710.562  28.9123
+   8 │ Plumas       709.556  13.2174
+   9 │ San Diego    659.436  12.2082
+  10 │ San Joaquin  551.189  11.578
+  11 │ Santa Clara  732.077  12.2291
 ```
 
-The `svyby` function can be used to generate stratified estimates. A vector of columns can be used to groupby. 
-
-```repl
-julia> using Survey      
-
-julia> data(api); 
-
-julia> dclus1 = svydesign(id=:dnum, weights=:pw, data = apiclus1, fpc=:fpc); 
-
-julia> svyby(:api00, [:cname, :meals], dclus1, svymean)
-```
+julia> svyby(:api00, [:cname, :meals], dclus1, svymean); 
 """
-function svyby(formula::Symbol, by::Vector{Symbol}, design::svydesign, func::Function, params = [])
-    gdf = groupby(design.data, by)
-    w = design.weights
-    return combine(gdf, [formula, w] => ((x, y) -> func(x, weights(y), params...)) => formula)
+function svyby(formula::Symbol, by, design::svydesign, func::Function, params = [])
+    gdf = groupby(design.variables, by)
+    return combine(gdf, [formula, :probs, :popsize, :sampsize] => ((a, b, c, d) -> func(a, b, c, d, params...)) => AsTable)
 end
--- src/svydesign.jl
+++ src/svydesign.jl
@@ -1,3 +1,10 @@
+function print_short(x)
+    if length(x) < 3
+        print(x)
+    else
+        print( x[1], ", ", x[2], ", ", x[3], " ...", " (length = ", length(x), ")")
+    end
+end
 """
 The `svydesign` object combines a data frame and all the survey design information needed to analyse it.
 
@@ -8,53 +15,94 @@ julia> data(api);
 
 julia> dclus1 = svydesign(id= :dnum, weights= :pw, data = apiclus1, fpc= :fpc) |> print
 Survey Design:
-data: 183x40 DataFrame
-weights: pw
+variables: 183x43 DataFrame
 id: dnum
-fpc: fpc
+strata:
+probs: 0.029544719150814778, 0.029544719150814778, 0.029544719150814778 ... (length = 183)
+fpc:
+    popsize: 757, 757, 757 ... (length = 183)
+    sampsize: 183, 183, 183 ... (length = 183)
 nest: false
-check_strat: false
+check_strat: true
 ```
 """
 struct svydesign
     id::Symbol
-    probs::Symbol
     strata::Symbol
-    variables::Symbol
-    fpc::Symbol
-    data::DataFrame
+    variables::DataFrame
     nest::Bool
     check_strat::Bool
-    weights::Symbol 
-    svydesign(; data = DataFrame(), id = Symbol(), probs = Symbol(), strata = Symbol(), variables = Symbol(), fpc = Symbol(), nest = false, check_strat = false, weights = Symbol()) = new(id, probs, strata, variables, fpc, data, nest, check_strat, weights)
 end
+
+function get_weights(data, wt::Vector)
+    if nrow(data) == length(wt)
+        return wt
+    else
+        @error "length of the weights vector is not equal to the number of rows in the dataset"
+    end
+end
+
+function get_weights(data, wt::Nothing)
+    return ones(nrow(data))
+end
+
+function get_weights(data, wt::Symbol)
+    wt = data[!, String(wt)]
+end
+
+function get_probs(data, wt, probs::Symbol)
+    return data[!, String(probs)]
+end
+
+function get_probs(data, wt, probs::Nothing)
+    return 1 ./ wt
+end
+
+function get_fpc(data, fpc::Symbol)
+    return data[!, String(fpc)]
+end
+
+function get_fpc(data, fpc::Nothing)
+    return repeat([nothing], nrow(data))
+end
+
+function get_fpc(data, fpc::Vector)
+    return fpc
+end
+
+function get_fpc(data, fpc::Real)
+    return repeat([fpc], nrow(data))
+end
+
+function svydesign(; data = DataFrame(), id = Symbol(), probs = nothing, strata = Symbol(), fpc = nothing, nest = false, check_strat = !nest, weights = nothing)
+    wt = get_weights(data, weights)
+    if isnothing(probs) & isnothing(weights)
+        @warn "No weights or probabilities supplied, assuming equal probability"
+    end
+    df = data
+    df.probs = ProbabilityWeights(get_probs(data, wt, probs))
+    df.popsize = get_fpc(data, fpc)
+    df.sampsize = repeat([nrow(data)], nrow(data))
+    return svydesign(id, strata, df, nest, check_strat)
+end 
 
 function Base.show(io::IO, design::svydesign)
     printstyled("Survey Design:\n") 
-    printstyled("data: "; bold = true)
-    print(size(design.data)[1], "x", size(design.data)[2], " DataFrame")
-    if length(string(design.weights)) > 0 
-        printstyled("\nweights: "; bold = true)
-        print(design.weights)
-    end
-    if length(string(design.id)) > 0
+    printstyled("variables: "; bold = true)
+    print(size(design.variables)[1], "x", size(design.variables)[2], " DataFrame")
         printstyled("\nid: "; bold = true)
         print(design.id)
-    end
-    if length(string(design.strata)) > 0
         printstyled("\nstrata: "; bold = true)
         print(design.strata)
-    end
-    if length(string(design.variables)) > 0
-        printstyled("\nid: "; bold = true)
-        print(design.variables)
-    end
-    if length(string(design.fpc)) > 0
+        printstyled("\nprobs: "; bold = true)
+        print_short(design.variables.probs)
         printstyled("\nfpc: "; bold = true)
-        print(design.fpc)
-    end
-    printstyled("\nnest: "; bold = true)
-    print(design.nest)
-    printstyled("\ncheck_strat: "; bold = true)
-    print(design.check_strat)
+        print("\n    popsize: ")
+        print_short(design.variables.popsize)
+        print("\n    sampsize: ")
+        print_short(design.variables.sampsize)
+        printstyled("\nnest: "; bold = true)
+        print(design.nest)
+        printstyled("\ncheck_strat: "; bold = true)
+        print(design.check_strat)
 end
