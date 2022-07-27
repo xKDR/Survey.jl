1dec83a91d65462e9b3446f9f19ce66bde5e20b4
Iulia Dumitru <iuliadmtru@gmail.com>
2022-07-27 11:40:00 +0300

    Import `freedman_diaconis` and `sturges` functions in the test file

--- test/svyhist.jl
+++ test/svyhist.jl
@@ -1,21 +1,14 @@
 using Survey
 using Test
 
+import Survey: freedman_diaconis, sturges
+
 @testset "svyhist.jl" begin
 	data(api)
 	dstrat = svydesign(data = apistrat, id = :1, strata = :stype, weights = :pw, fpc = :fpc)
 
-	sturges(n::Integer) = ceil(Int, log2(n)) + 1
-	sturges(vec::AbstractVector) = ceil(Int, log2(length(vec))) + 1
-	sturges(df::DataFrame, var::Symbol) = ceil(Int, log2(size(df[!, var], 1))) + 1
-	sturges(design::svydesign, var::Symbol) = sturges(design.variables, var)
-
 	@test sturges(10) == 5
 	@test sturges([1, 2, 5, 10, 15, 17, 20]) == 4
-
-	freedman_diaconis(v::AbstractVector) = round(Int, length(v)^(1 / 3) * (maximum(v) - minimum(v)) / (2 * iqr(v)))
-	freedman_diaconis(df::DataFrame, var::Symbol) = freedman_diaconis(df[!, var])
-	freedman_diaconis(design::svydesign, var::Symbol) = freedman_diaconis(design.variables[!, var])
 
 	h = svyhist(dstrat, :enroll)
 	@test getindex(h.plot.bins) == 15
