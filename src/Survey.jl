1711c69e0645711b458f84669b7460b89177192e
Iulia Dumitru <iuliadmtru@gmail.com>
2022-07-29 20:49:47 +0300

    Implement `dimnames`, `dim` and `colnames`

--- src/Survey.jl
+++ src/Survey.jl
@@ -13,12 +13,14 @@ include("svyby.jl")
 include("example.jl")
 include("svyglm.jl")
 include("svyhist.jl")
+include("dimnames.jl")
 
 export svydesign, svyby, svyglm
 export data, api, apiclus1, apiclus2, apipop, apistrat, apisrs
 export svymean, svytotal, svyquantile
 export @formula
 export svyhist, sturges, freedman_diaconis
+export dim, colnames, dimnames
 export
     #families
     Normal,
