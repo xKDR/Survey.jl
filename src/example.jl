eb645042ce5ce37eba81b6a775e7cf07a0e64752
Fabian Greimel <fabgrei@gmail.com>
2022-08-03 16:46:14 +0200

    loading one file at a time

--- src/example.jl
+++ src/example.jl
@@ -1,3 +1,6 @@
+const PKG_DIR = joinpath(pathof(Survey), "..", "..") |> normpath
+asset_path(file) = joinpath(PKG_DIR, "assets", file)
+
 """
 The Academic Performance Index is computed for all California schools based on standardised testing of students.
 The data sets contain information for all schools with at least 100 students and for various probability samples of the data.
@@ -16,21 +19,7 @@ Details about the columns of the dataset can be found here: https://r-survey.r-f
 The API program has been discontinued at the end of 2018. Information is archived at https:
 //www.cde.ca.gov/re/pr/api.asp
 """
-struct API
-    filenames
-end
-
-api = API(["apiclus1", "apiclus2", "apipop"])
-
-const PKG_DIR = joinpath(pathof(Survey), "..", "..") |> normpath
-asset_path(file) = joinpath(PKG_DIR, "assets", file)
-
-function load_data(dataset::API)
-    apiclus1 = CSV.read(asset_path("apiclus1.csv"), DataFrame, missingstring="NA")
-    apiclus2 = CSV.read(asset_path("apiclus2.csv"), DataFrame, missingstring="NA")
-    apipop   = CSV.read(asset_path("apipop.csv"),   DataFrame, missingstring="NA")
-    apistrat = CSV.read(asset_path("apistrat.csv"), DataFrame, missingstring="NA")
-    apisrs   = CSV.read(asset_path("apisrs.csv"),   DataFrame, missingstring="NA")
-
-    (; apiclus1, apiclus2, apipop, apistrat, apisrs)
+function load_data(name)
+    @assert name ∈ ["apiclus1", "apiclus2", "apipop", "apistrat", "apisrs"]
+    CSV.read(asset_path(name * ".csv"), DataFrame, missingstring="NA")
 end
