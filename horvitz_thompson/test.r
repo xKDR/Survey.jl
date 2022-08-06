3e7fadc94a73d0324bd5bb5057d825a1f6877583
harsharora21 <harsharora1337@gmail.com>
2022-08-06 16:37:13 +0530

    moved horvitz code to src

--- horvitz_thompson/test.r
+++
@@ -1,6 +0,0 @@
-library(survey)
-
-data(api)
-dclus1 <- svydesign(id = ~dnum, weights = ~pw, data = apiclus1, fpc = ~fpc)
-svyby(~api00, by = ~cname, design = dclus1, svymean, keep.var = FALSE)
-svyby(~api00, by = ~cname, design = dclus1, svymean)
