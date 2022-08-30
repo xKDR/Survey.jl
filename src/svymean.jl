e78b98a3d6cb0c3867b124663ed694ca5607d415
Iulia Dumitru <iuliadmtru@gmail.com>
2022-08-30 19:00:13 +0300

    Change doctest to correct output, `sem` instead of `SEM`

--- src/svymean.jl
+++ src/svymean.jl
@@ -8,7 +8,7 @@ julia> srs = SimpleRandomSample(apisrs);
 
 julia> svymean(:enroll, srs)
 1×2 DataFrame
- Row │ mean     SE
+ Row │ mean     sem
      │ Float64  Float64
 ─────┼──────────────────
    1 │  584.61  27.8212
