dd9fb439482d392b5c1a6cccb0c533041a073043
Iulia Dumitru <iuliadmtru@gmail.com>
2022-09-05 14:53:52 +0300

    Add `svytotal` method for `svyby` and change `svyby` accordingly

--- src/svymean.jl
+++ src/svymean.jl
@@ -18,6 +18,9 @@ function var_of_mean(x::Symbol, design::SimpleRandomSample)
     return design.fpc / design.sampsize * var(design.data[!, x])
 end
 
+"""
+Inner method for `svyby`.
+"""
 function var_of_mean(x::AbstractVector, design::SimpleRandomSample)
     return design.fpc / design.sampsize * var(x)
 end
@@ -26,14 +29,21 @@ function sem(x, design::SimpleRandomSample)
     return sqrt(var_of_mean(x, design))
 end
 
+"""
+Inner method for `svyby`.
+"""
 function sem(x::AbstractVector, design::SimpleRandomSample)
     return sqrt(var_of_mean(x, design))
 end
 
-function svymean(x, design::SimpleRandomSample)
+function svymean(x::Symbol, design::SimpleRandomSample)
     return DataFrame(mean = mean(design.data[!, x]), sem = sem(x, design::SimpleRandomSample))
 end
 
-function svymean(x::AbstractVector , design::SimpleRandomSample)
+"""
+Inner method for `svyby`.
+"""
+# TODO: results not matching for `sem`
+function svymean(x::AbstractVector, design::SimpleRandomSample, _)
     return DataFrame(mean = mean(x), sem = sem(x, design::SimpleRandomSample))
 end
--- src/svytotal.jl
+++ src/svytotal.jl
@@ -20,13 +20,36 @@ function var_of_total(x::Symbol, design::SimpleRandomSample)
     return design.popsize^2 * design.fpc / design.sampsize * var(design.data[!, x])
 end
 
+"""
+Inner method for `svyby`.
+"""
+function var_of_total(x::AbstractVector, design::SimpleRandomSample)
+    return design.popsize^2 * design.fpc / design.sampsize * var(x)
+end
+
 function se_tot(x::Symbol, design::SimpleRandomSample)
+    return sqrt(var_of_total(x, design))
+end
+
+"""
+Inner method for `svyby`.
+"""
+function se_tot(x::AbstractVector, design::SimpleRandomSample)
     return sqrt(var_of_total(x, design))
 end
 
 function svytotal(x::Symbol, design::SimpleRandomSample)
     # total = design.pop_size * mean(design.data[!, variable])
-    total = wsum(design.data[!, x] , weights(design.data.weights)  )
+    total = wsum(design.data[!, x] , weights(design.data.weights))
+    return DataFrame(total = total , se_total = se_tot(x, design::SimpleRandomSample))
+end
+
+"""
+Inner method for `svyby`.
+"""
+# TODO: results not matching for `sem`
+function svytotal(x::AbstractArray, design::SimpleRandomSample, wts)
+    total = wsum(x, weights(wts))
     return DataFrame(total = total , se_total = se_tot(x, design::SimpleRandomSample))
 end
 
