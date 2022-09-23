5f398c7546322fb9003dfdfc7b7cf89e8f2abca2
smishr <sm.oz@outlook.com>
2022-09-23 17:01:05 +0530

    Add sbytotal for Stratified

--- src/svytotal.jl
+++ src/svytotal.jl
@@ -31,9 +31,9 @@ function se_tot(x::Symbol, design::SimpleRandomSample)
     return sqrt(var_of_total(x, design))
 end
 
-function total(x::Symbol, design::SimpleRandomSample)
-    return wsum(design.data[!, x] , weights(design.data.weights)  )
-end
+# function total(x::Symbol, design::SimpleRandomSample)
+#     return wsum(design.data[!, x] , weights(design.data.weights)  )
+# end
 
 function svytotal(x::Symbol, design::SimpleRandomSample)
     # Support behaviour like R for CategoricalArray type data
@@ -55,3 +55,22 @@ end
 # function svytotal(x::Symbol, design::svydesign)
 #     # TODO
 # end
+
+function svytotal(x::Symbol, design::StratifiedSample)
+    # # Support behaviour like R for CategoricalArray type data
+    # if isa(x,Symbol) && isa(design.data[!,x], CategoricalArray)
+    #     gdf = groupby(design.data, x)
+    #     p = combine(gdf, nrow => :count )
+    #     p.total = design.popsize .* p.count ./ sum(p.count)
+    #     p.proportion = p.total ./ design.popsize
+    #     p = select!(p, Not(:count)) # Drop the count column as not really desired for svytotal
+    #     p.var = design.popsize^2 .* design.fpc .* p.proportion .* (1 .- p.proportion) ./ (design.sampsize - 1) # N^2 .* Formula for variance of proportion
+    #     p.se = sqrt.(p.var)
+    #     return p
+    # end
+    gdf = groupby(design.data,design.strata)
+    # wsum(design.data[!, x] , weights(design.data.weights)  )
+    grand_total = sum(combine(gdf, [x,:weights] => ( (a,b) -> wsum(a,b) ) => :total).total)
+    # grand_total = wsum( combine(gdf, x => mean => :mean).mean, weights(combine(gdf, :weights => sum => :Nₕ ).Nₕ ) )
+    return DataFrame(grand_total = grand_total) # , sem = sem(x, design::SimpleRandomSample))
+end
