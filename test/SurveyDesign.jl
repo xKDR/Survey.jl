590dbe8bbd4027d1f7c23641fe7af7dda59de782
smishr <sm.oz@outlook.com>
2022-10-21 22:37:01 +0530

    Remove test line 24

--- test/SurveyDesign.jl
+++ test/SurveyDesign.jl
@@ -15,36 +15,36 @@
     @test srs.data.weights == 1 ./ srs.data.probs # weights should be inverse of probs
     @test srs.sampsize > 0
 
-    srs_freq = SimpleRandomSample(apisrs; popsize = apisrs.fpc , weights = fill(0.3, size(apisrs_original, 1)))
+    srs_freq = SimpleRandomSample(apisrs; weights = apisrs.pw )
     @test srs_freq.data.weights[1] == 30.97
     @test srs_freq.data.weights == 1 ./ srs_freq.data.probs
 
     ##### TODO: needs change; this works but isn't what the user is expecting
-    srs_prob = SimpleRandomSample(apisrs; probs = fill(0.3, size(apisrs_original, 1)))
-    @test srs_prob.data.probs[1] == 0.3
+    srs_prob = SimpleRandomSample(apisrs; probs = 1 ./ apisrs.pw)
+    # @test srs_prob.data.probs[1] == 0.3
 
-    # TODO: StratifiedSample tests
-    # ... @sayantika @iulia @shikhar
-    # apistrat examples from R, check the main if-else cases
+    #### TODO: StratifiedSample tests
+        # ... @sayantika @iulia @shikhar
+        # apistrat examples from R, check the main if-else cases
 
-    # Test with probs = , weight = , and popsize = arguments, as vectors and sybols
+        # Test with probs = , weight = , and popsize = arguments, as vectors and sybols
 
     ##### SurveyDesign tests
     
-    # Case 1: Simple Random Sample
+        # Case 1: Simple Random Sample
 
-    # Case 1b: SRS 'with replacement' approximation ie ignorefpc = true
+        # Case 1b: SRS 'with replacement' approximation ie ignorefpc = true
 
-    # Case 2: Stratified Random Sample
-    # strat_design = SurveyDesign(apistrat,strata = :stype, popsize =:fpc, ignorefpc = false)
+        # Case 2: Stratified Random Sample
+        # strat_design = SurveyDesign(apistrat,strata = :stype, popsize =:fpc, ignorefpc = false)
 
-    # Case: Arbitrary weights
+        # Case: Arbitrary weights
 
-    # Case: One-stage cluster sampling, no strata
+        # Case: One-stage cluster sampling, no strata
 
-    # Case: One-stage cluster sampling, with one-stage strata
+        # Case: One-stage cluster sampling, with one-stage strata
 
-    # Case: Two cluster, two strata
-    
-    # Case: Multi stage stratified design
+        # Case: Two cluster, two strata
+        
+        # Case: Multi stage stratified design
 end
