5c929040d3bb38430a9cd8bb418369c3b02cf0b0
smishr <sm.oz@outlook.com>
2022-10-21 22:36:00 +0530

    Fix err sampsize>popsize, Add C;usterSample class

--- src/SurveyDesign.jl
+++ src/SurveyDesign.jl
@@ -53,6 +53,7 @@ struct SimpleRandomSample <: AbstractSurveyDesign
             end
             # estimate population size
             popsize = round(sampsize * first(weights)) |> UInt
+            @show popsize, sampsize
             if sampsize > popsize
                 error("sample size cannot be greater than population size")
             end
@@ -83,7 +84,7 @@ end
 """
     StratifiedSample <: AbstractSurveyDesign
 
-Survey design sampled by stratification.
+    Survey design sampled by stratification.
 """
 struct StratifiedSample <: AbstractSurveyDesign
     data::AbstractDataFrame
@@ -148,6 +149,78 @@ struct StratifiedSample <: AbstractSurveyDesign
         new(data, strata, sampsize, popsize, sampfraction, fpc, ignorefpc)
     end
 end
+
+"""
+    ClusterSample <: AbstractSurveyDesign
+
+    One stage clusters, Primary Sampling Units (PSU) chosen with SRS. No stratification.
+    Assuming every individual is in one and only one cluster, and the subsampling probabilities
+    for any given cluster do not depend on which other clusters were sampled. (Lumley pg41, para2)
+"""
+struct ClusterSample <: AbstractSurveyDesign
+    data::AbstractDataFrame
+    clusters::Union{Symbol,Nothing}
+    sampsize::Union{Nothing,Vector{Real}}
+    popsize::Union{Nothing,Vector{Real}}
+    sampfraction::Vector{Real}
+    fpc::Vector{Real}
+    ignorefpc::Bool
+    function StratifiedSample(data::AbstractDataFrame, strata::Symbol;
+        popsize=nothing,
+        sampsize=transform(groupby(data, strata), nrow => :counts).counts,
+        weights=nothing,
+        probs=nothing,
+        ignorefpc=false
+    )
+        if isa(weights, Symbol)
+            weights = data[!, weights]
+        end
+        if isa(probs, Symbol)
+            probs = data[!, probs]
+        end
+
+        if ignorefpc
+            # TODO: change what happens if `ignorepfc == true` or if the user only
+            # specifies `data`
+            @warn "assuming equal weights"
+            weights = ones(nrow(data))
+        end
+
+        # set population size if it is not given; `weights` and `sampsize` must be given
+        if isnothing(popsize)
+            # TODO: add probability weights if `weights` is not `nothing`
+            if typeof(probs) <: Vector{<:Real}
+                weights = 1 ./ probs
+            end
+            # estimate population size
+            popsize = sampsize .* weights
+
+            if sampsize > popsize
+                error("sample size cannot be greater than population size")
+            end
+        elseif typeof(popsize) <: Vector{<:Real}
+            # TODO: change `elseif` condition
+            weights = popsize ./ sampsize # expansion estimator
+        # TODO: add probability weights
+        else
+            error("either population size or frequency/probability weights must be specified")
+        end
+        # set sampling fraction
+        sampfraction = sampsize ./ popsize
+        # set fpc
+        fpc = ignorefpc ? 1 : 1 .- (sampsize ./ popsize)
+        # add columns for frequency and probability weights to `data`
+        if !isnothing(probs)
+            data[!, :probs] = probs
+            data[!, :weights] = 1 ./ data[!, :probs]
+        else
+            data[!, :weights] = weights
+            data[!, :probs] = 1 ./ data[!, :weights]
+        end
+        new(data, strata, sampsize, popsize, sampfraction, fpc, ignorefpc)
+    end
+end
+
 
 """
     SurveyDesign <: AbstractSurveyDesign
