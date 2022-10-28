123fb6073549c1ec0cc4635ae97f40f51a51a60e
Sayantika Sengupta <sayantikasenguptassg@gmail.com>
2022-10-28 15:05:50 +0530

    Add SRS tests

--- test/SurveyDesign.jl
+++ test/SurveyDesign.jl
@@ -1,4 +1,4 @@
-@testset "SurveyDesign.jl" begin
+@testset "SimpleRandomSample" begin
     # SimpleRandomSample tests
     apisrs_original = load_data("apisrs")
     apisrs = copy(apisrs_original)
@@ -14,6 +14,25 @@
     # TODO: needs change; this works but isn't what the user is expecting
     srs_prob = SimpleRandomSample(apisrs; probs = fill(0.3, size(apisrs_original, 1)))
     @test srs_prob.data.probs[1] == 0.3
+    @test srs_prob.data.weights == 1 ./ srs_prob.data.probs
 
-    # StratifiedSample tests
+    srs = SimpleRandomSample(apisrs, ignorefpc = true, weights = :pw )
+    @test srs.data.probs == 1 ./ srs.data.weights
+    
+    srs = SimpleRandomSample(apisrs, ignorefpc = false, weights = :pw)
+    @test srs.data.probs == 1 ./ srs.data.weights
+
+    srs_weights = SimpleRandomSample(apisrs, ignorefpc = false, weights = :fpc)
+    @test_throws srs_weights = SimpleRandomSample(apisrs, ignorefpc = false, weights = :stype)
+    srs_w_p = SimpleRandomSample(apisrs, ignorefpc = false, weights = :fpc, probs = fill(0.3, size(apisrs_original, 1)))
+
+    srs = SimpleRandomSample(apisrs, ignorefpc = true, probs = 1 ./ apisrs.pw )
+    @test srs.data.probs == 1 ./ srs.data.weights
+    srs = SimpleRandomSample(apisrs, popsize = -2.8, ignorefpc = true)# the errror is wrong
+    srs = SimpleRandomSample(apisrs, sampsize = -2.8, ignorefpc = true)# the function is working upto line 55
+
+end
+
+@testset "StratifiedSample" begin
+  # StratifiedSample tests   
 end
