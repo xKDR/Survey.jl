8de236e411aceb6cc1a7e4944c770d927400b7e4
Ayush Patnaik <u6012645@anu.edu.au>
2022-12-02 20:45:50 +0530

    Update src/SurveyDesign.jl
    Co-authored-by: Iulia Dumitru <84318573+iuliadmtru@users.noreply.github.com>

--- src/SurveyDesign.jl
+++ src/SurveyDesign.jl
@@ -209,7 +209,7 @@ The `popsize`, `weights` and `probs` parameters follow the same rules as for [`S
 ```jldoctest
 julia> apistrat = load_data("apistrat");
 
-julia> dstrat = StratifiedSample(apistrat, :stype; popsize=:fpc);
+julia> dstrat = StratifiedSample(apistrat, :stype; popsize=:fpc)
 julia> dstrat
 StratifiedSample:
 data: 200x45 DataFrame
