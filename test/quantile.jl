346e44426d39c563ea1688ce8ffe226b0b5fe4f6
smishr <43640926+smishr@users.noreply.github.com>
2022-12-06 16:51:02 +0530

    quantile tests updated template

--- test/quantile.jl
+++ test/quantile.jl
@@ -8,18 +8,16 @@
     ### weights or probs as Symbol
     apisrs = copy(apisrs_original)
     srs_design = SimpleRandomSample(apisrs,popsize=:fpc)
-    
+end
 
+@testset "quantile_Stratified" begin
+    ## Add tests
+end
 
-    # srs_old = design(id = :1, data = apisrs)
-    # 0.5th percentile
-    q_05_new = quantile(:api00, srs_new, 0.5)
-    # q_05_old = quantile(:api00, srs_old, 0.5)
-    @test q_05_new == q_05_old
-    # 0.25th percentile
-    q_025_new = quantile(:api00, srs_new, 0.25)
-    # q_025_old = quantile(:api00, srs_old, 0.25)
-    # @test q_025_new == q_025_old
+@testset "quantile_by_SimpleRandomSample" begin
+    ## Add tests
+end
 
-    # StratifiedSample
+@testset "quantile_by_Stratified" begin
+    ## Add tests
 end
