5d665f982e419c560d313689b78fc05a4c5bab1f
smishr <43640926+smishr@users.noreply.github.com>
2022-12-06 16:46:11 +0530

    updated quantile

--- src/quantile.jl
+++ src/quantile.jl
@@ -1,6 +1,14 @@
 """
-    quantile(var, design, q)
-Estimate quantiles for `SurveyDesign`s.
+    quantile(var, design, q; kwargs...)
+Estimate quantiles for a complex survey.
+
+Hyndman and Fan compiled a taxonomy of nine algorithms to estimate quantiles. These are implemented in Statistics.quantile, which this function calls.
+The Julia, R and Python-numpy use the same defaults
+
+# References:
+Hyndman, R.J and Fan, Y. (1996) "Sample Quantiles in Statistical Packages", The American Statistician, Vol. 50, No. 4, pp. 361-365
+[Quantiles](https://en.m.wikipedia.org/wiki/Quantile) on wikipedia
+Section 2.4.1 and Appendix C.4 - [Complex Surveys: a guide to analysis using R](https://r-survey.r-forge.r-project.org/svybook/)
 
 ```jldoctest
 julia> apisrs = load_data("apisrs");
@@ -13,36 +21,33 @@ julia> quantile(:enroll, srs, 0.5)
      │ Float64
 ─────┼──────────────────
    1 │            453.0
+
+julia> quantile(:enroll, srs, [0.25,0.75, 0.99])
+3×1 DataFrame
+ Row │ [0.25, 0.75, 0.99]th percentile 
+     │ Float64                         
+─────┼─────────────────────────────────
+   1 │                          339.0
+   2 │                          668.5
+   3 │                         1911.39
+
+julia> strat = load_data("apistrat");
+
+julia> dstrat = StratifiedSample(strat, :stype; popsize=:fpc);
+
+julia> quantile(:enroll, dstrat, [0.1,0.2,0.5,0.75,0.95])
+
 ```
 """
-function quantile(var, design::SimpleRandomSample, q; kwargs...)
+function quantile(var::Symbol, design::SimpleRandomSample, q::Union{<:Real,Vector{<:Real}}; alpha::Real=1.0, beta::Real=alpha, kwargs...)
     x = design.data[!, var]
-    df = DataFrame(tmp = Statistics.quantile(Float32.(x), q; kwargs...))
-    rename!(df, :tmp => Symbol(string(q) .* "th percentile"))
+    df = DataFrame(qth_quantile = q, quantile = Statistics.quantile(Float32.(x), q; kwargs...))
     return df
 end
 
-function quantile(var, design::StratifiedSample, q)
+function quantile(var::Symbol, design::StratifiedSample, q::Union{<:Real,Vector{<:Real}}; kwargs...)
     x = design.data[!, var]
     w = design.data.probs
-    df = DataFrame(tmp = Statistics.quantile(Float32.(x), weights(w), q))
-    rename!(df, :tmp => Symbol(string(q) .* "th percentile"))
-    return df
-end
-
-function quantile(var, design::design, q)
-    x = design.variables[!, var]
-    w = design.variables.probs
-    df = DataFrame(tmp = Statistics.quantile(Float32.(x), weights(w), q))
-    rename!(df, :tmp => Symbol(string(q) .* "th percentile"))
-
-    return df
-end
-
-# Inner method for `by`
-function quantile(x, w, _, q)
-    df = DataFrame(tmp = Statistics.quantile(Float32.(x), weights(w), q))
-    rename!(df, :tmp => Symbol(string(q) .* "th percentile"))
-
+    df = DataFrame(qth_quantile = q, quantile = Statistics.quantile(Float32.(x), weights(w), q))
     return df
 end
