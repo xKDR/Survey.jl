126c4115ad89f8491218be5147e5b95f374fd016
smishr <43640926+smishr@users.noreply.github.com>
2022-12-09 12:17:25 +0530

    Add Clustersample, replace strata->cluster

--- src/SurveyDesign.jl
+++ src/SurveyDesign.jl
@@ -321,3 +321,140 @@ struct StratifiedSample <: AbstractSurveyDesign
         new(data, strata, ignorefpc)
     end
 end
+
+"""
+    ClusterSample <: AbstractSurveyDesign
+
+Survey design sampled by two stage cluster sampling. Assumes each individual in one and only one cluster; disjoint and nested clusters.
+
+`cluster` must be specified as a Symbol name of a column in `data`.
+
+# Arguments:
+`data::AbstractDataFrame`: the survey dataset (!this gets modified by the constructor).
+`cluster::Symbol`: the stratification variable - must be given as a column in `data`.
+`sampsize::Union{Nothing,Symbol,<:Unsigned,Vector{<:Real}}=UInt(nrow(data))`:  the survey sample size.
+`popsize::Union{Nothing,Symbol,<:Unsigned,Vector{<:Real}}=nothing`: the (expected) survey population size.
+`weights::Union{Nothing,Symbol,Vector{<:Real}}=nothing`: the sampling weights.
+`probs::Union{Nothing,Symbol,Vector{<:Real}}=nothing: the sampling probabilities.
+`ignorefpc::Bool=false`: choose to ignore finite population correction and assume all weights equal to 1.0
+
+The `popsize`, `weights` and `probs` parameters follow the same rules as for [`SimpleRandomSample`](@ref).
+
+"""
+struct ClusterSample <: AbstractSurveyDesign
+    data::AbstractDataFrame
+    cluster::Symbol
+    ignorefpc::Bool
+    function ClusterSample(data::AbstractDataFrame, cluster::Symbol;
+        popsize::Union{Nothing,Symbol}=nothing,
+        sampsize::Union{Nothing,Symbol}=nothing,
+        weights::Union{Nothing,Symbol,Vector{<:Real}}=nothing,
+        probs::Union{Nothing,Symbol,Vector{<:Real}}=nothing,
+        ignorefpc::Bool=false
+    )
+        # Store the iterator over each cluster, as used multiple times
+        data_groupedby_cluster = groupby(data, cluster)
+        # If any of weights or probs given as Symbol, find the corresponding column in `data`
+        if isa(weights, Symbol)
+            for each_cluster in keys(data_groupedby_cluster)
+                if !all(w -> w == first(data_groupedby_cluster[each_cluster][!, weights]), data_groupedby_cluster[each_cluster][!, weights])
+                    error("sampling weights within each cluster must be equal in ClusterSample")
+                end
+            end
+            # original_weights_colname = copy(weights)
+            weights = data[!, weights] # If all good with weights column, then store it as Vector
+        end
+        if isa(probs, Symbol)
+            for each_cluster in keys(data_groupedby_cluster)
+                if !all(p -> p == first(data_groupedby_cluster[each_cluster][!, probs]), data_groupedby_cluster[each_cluster][!, probs])
+                    error("sampling probabilities within each cluster must be equal in ClusterSample")
+                end
+            end
+            # original_probs_colname = copy(probs)
+            probs = data[!, probs] # If all good with probs column, then store it as Vector
+        end
+        # If weights/probs vector not numeric/real, ie. string column passed for weights, then raise error
+        if !isa(weights, Union{Nothing,Vector{<:Real}})
+            error("weights should be Vector{<:Real}. You passed $(typeof(weights))")
+        elseif !isa(probs, Union{Nothing,Vector{<:Real}})
+            error("sampling probabilities should be Vector{<:Real}. You passed $(typeof(probs))")
+        end
+        # If popsize given as Symbol or Vector, check all records equal in each cluster
+        if isa(popsize, Symbol)
+            for each_cluster in keys(data_groupedby_cluster)
+                if !all(w -> w == first(data_groupedby_cluster[each_cluster][!, popsize]), data_groupedby_cluster[each_cluster][!, popsize])
+                    error("popsize must be same for all observations within each cluster in ClusterSample")
+                end
+            end
+            # original_popsize_colname = copy(popsize)
+            popsize = data[!, popsize]
+        end
+        # If sampsize given as Symbol or Vector, check all records equal 
+        if isa(sampsize, Symbol)
+            if isnothing(popsize) && isnothing(weights) && isnothing(probs)
+                error("if sampsize given, and popsize not given, then weights or probs must given to calculate popsize")
+            end
+            for each_cluster in keys(data_groupedby_cluster)
+                if !all(w -> w == first(data_groupedby_cluster[each_cluster][!, sampsize]), data_groupedby_cluster[each_cluster][!, sampsize])
+                    error("sampsize must be same for all observations within each cluster in ClusterSample")
+                end
+            end
+            # original_sampsize_colname = copy(sampsize)
+            sampsize = data[!, sampsize]
+            # If sampsize column not provided in constructor call, set it as nrow of cluster
+        elseif isnothing(sampsize)
+            sampsize = transform(groupby(data, cluster), nrow => :counts).counts
+        end
+        # If both `weights` and `probs` given, then `weights` is assumed to be ground truth for probs.
+        if !isnothing(weights) && !isnothing(probs)
+            probs = 1 ./ weights
+            data[!, :probs] = probs
+        end
+        # `popsize` is either nothing or a Vector{<:Real} by now
+        if isnothing(popsize)
+            # If popsize not given, fallback to weights, probs and sampsize to estimate `popsize`
+            @warn "popsize not given. using weights/probs and sampsize to estimate `popsize` for ClusterSample"
+            # Check that all weights (or probs if weights not given) are equal, as SRS is by definition equi-weighted
+            if typeof(probs) <: Vector{<:Real}
+                weights = 1 ./ probs
+            elseif !(typeof(weights) <: Vector{<:Real})
+                error("either weights or probs must be given if `popsize` not given")
+            end
+            # Estimate population size
+            popsize = sampsize .* weights
+        elseif typeof(popsize) <: Vector{<:Real} # Still need to check if the provided Column is of <:Real
+            # If popsize is given, weights and probs made concordant with popsize and sampsize, regardless of supplied arguments
+            weights = popsize ./ sampsize
+            probs = 1 ./ weights
+        else
+            error("something went wrong. Please check validity of inputs.")
+        end
+        # If sampsize greater than popsize than illogical arguments specified.
+        if any(sampsize .> popsize)
+            error("population sizes were estimated to be less than sampsize. please check input arguments.")
+        end
+        # If ignorefpc then set weights to 1 ??
+        # TODO: This works under some cases, but should find better way to process ignoring fpc
+        if ignorefpc
+            @warn "assuming all weights are equal to 1.0"
+            weights = ones(nrow(data))
+            probs = 1 ./ weights
+        end
+        ## Set remaining parts of data structure
+        # set sampling fraction
+        sampfraction = sampsize ./ popsize
+        # set fpc
+        fpc = ignorefpc ? fill(1, size(data, 1)) : 1 .- (sampsize ./ popsize)
+        # add columns for frequency and probability weights to `data`
+        data[!, :weights] = weights
+        if isnothing(probs)
+            probs = 1 ./ data[!, :weights]
+        end
+        data[!, :probs] = probs
+        data[!, :sampsize] = sampsize
+        data[!, :popsize] = popsize
+        data[!, :fpc] = fpc
+        data[!, :sampfraction] = sampfraction
+        new(data, cluster, ignorefpc)
+    end
+end
