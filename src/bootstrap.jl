62dc3fd8023f5eb1e5805ca38814e83fe127d171
ayushpatnaikgit <ayushpatnaik@gmail.com>
2023-01-01 17:36:37 +0530

    Fix bug in bootstrap

--- src/bootstrap.jl
+++ src/bootstrap.jl
@@ -1,63 +1,28 @@
-struct Bootstrap 
-    replicates
-    rng
-    function Bootstrap(; replicates = 1000, rng = MersenneTwister(111))
-        new(replicates, rng)
-    end
-end
-
 """
 ```jldoctest
-julia> using Survey, Random, StatsBase; 
+julia> using Survey, Random;
 
 julia> apiclus1 = load_data("apiclus1"); 
 
-julia> dclus1 = SurveyDesign(apiclus1, :dnum, :fpc); 
+julia> dclus1 = SurveyDesign(apiclus1; clusters = :dnum); 
 
 julia> rng = MersenneTwister(111); 
 
-julia> func = wsum; 
-
-julia> Survey.bootstrap(:api00, dclus1, func; replicates=1000, rng) 
-1×2 DataFrame
- Row │ statistic  SE        
-     │ Float64    Float64   
-─────┼──────────────────────
-   1 │ 5.94916e6  1.36593e6
-
+julia> Survey.bootweights(dclus1; replicates=1000, rng) 
+Survey.ReplicateDesign:
+data: 183x1046 DataFrame
+cluster: dnum
+design.data[!,design.cluster]: 637, 637, 637, ..., 448
+popsize: popsize
+design.data[!,design.popsize]: 183, 183, 183, ..., 183
+sampsize: sampsize
+design.data[!,design.sampsize]: 15, 15, 15, ..., 15
+design.data[!,:probs]: 1.0, 1.0, 1.0, ..., 1.0
+design.data[!,:allprobs]: 1.0, 1.0, 1.0, ..., 1.0
+replicates: 1000
 ```
 """
-function bootstrap(x::Symbol, design::SurveyDesign, func = wsum; replicates = 100, rng = MersenneTwister(1234))
-    X = func(design.data[:, x], design.data.weights)
-    H = length(unique(design.data[!, design.strata]))
-    stratified = groupby(design.data, design.strata)
-    Xt = Array{Float64, 1}(undef, replicates)
-    for i in 1:replicates
-        Xh = []
-        Wh = []
-        for j in 1:H
-            substrata = DataFrame(stratified[j])
-            psus = unique(substrata[!, design.cluster])
-            if length(psus) == 1
-                return DataFrame(statistic = X, SE = 0)
-            end
-            nh = length(psus)
-            gdf = groupby(substrata, design.cluster)
-            selected_psus = psus[rand(rng, 1:nh, (nh-1))] # simple random sample of PSUs, with replacement. Select (nh-1) out of nh
-            xhij = (reduce(vcat, [gdf[(i,)][!, x] for i in selected_psus]))
-            whij = (reduce(vcat, [gdf[(i,)].weights * (nh / (nh - 1)) for i in selected_psus]))
-            append!(Xh, xhij)
-            append!(Wh, whij)
-        end
-        Xh = Float64.(Xh)
-        Wh = Float64.(Wh)
-        Xt[i] = func(Xh, Wh)
-        end 
-        variance = sum((Xt .- X).^2) / replicates
-    return DataFrame(statistic = X, SE = sqrt(variance))
-end
-
-function bootweights(design::SurveyDesign; replicates = 100, rng = MersenneTwister(1234))
+function bootweights(design::SurveyDesign; replicates = 4000, rng = MersenneTwister(1234))
     H = length(unique(design.data[!, design.strata]))
     stratified = groupby(design.data, design.strata)
     function replicate(stratified, H)
@@ -68,7 +33,8 @@ function bootweights(design::SurveyDesign; replicates = 100, rng = MersenneTwist
                 return DataFrame(statistic = X, SE = 0)
             end
             nh = length(psus)
-            rh = [(count(==(i), rand(rng, 1:(nh-1), nh))) for i in 1:nh] # main bootstrap algo. 
+            randinds = rand(rng, 1:(nh), (nh-1)) # Main bootstrap algo. Draw nh-1 out of nh, with replacement.  
+            rh = [(count(==(i), randinds)) for i in 1:nh] # main bootstrap algo. 
             gdf = groupby(substrata, design.cluster)
             for i in 1:nh
                 gdf[i].rh = repeat([rh[i]], nrow(gdf[i]))
