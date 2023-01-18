4342578dd79907fc844405765f8bae0df2600b0c
sayantikaSSG <sayantikasenguptassg@gmail.com>
2023-01-18 13:54:34 +0530

    commiting the scratch of jackknife

--- src/jackknife.jl
+++ src/jackknife.jl
@@ -18,3 +18,32 @@ function jkknife(variable:: Symbol, design::OneStageClusterSample ,func:: Functi
     var = c*(nh-1)/nh
     return DataFrame(Statistic = statistic, SE = sqrt(var))
 end
+
+function jackknifeweights(design::SurveyDesign )
+    H = length(unique(design.data[!, design.strata]))
+    stratified = groupby(design.data, design.strata)
+    for j in 1:H
+        substrata = DataFrame(stratified[j])
+        psus = unique(substrata[!, design.cluster])
+        if length(psus) == 1
+            return DataFrame(statistic = X, SE = 0)
+        end
+        newv=[]
+        nh = length(psus)
+        gdf = groupby(substrata, design.cluster)
+        replicates = [filter(n -> n != i, 1:nh) for i in 1:nh]
+        clusterweights = []
+        for replicate in replicates
+            push!(clusterweights ,[(count(==(i), replicate)) for i in 1:nh].*(nh/(nh-1)) )# main jackknife algo. 
+        end
+        for j in 1:nh 
+            clusterweight = clusterweights[j]
+            for i in 1:nh 
+                gdf[i][!,"replicate_"*string(j)] = clusterweight[i].*DataFrame(gdf[i]).weights
+            end
+        end
+        stratified[j].replicate_1 = DataFrame(gdf).replicate_1
+    end
+    DataFrame(stratified)
+    return ReplicateDesign(DataFrame(stratified), design.cluster, design.popsize, design.sampsize, design.strata, design.pps, replicates) 
+end
