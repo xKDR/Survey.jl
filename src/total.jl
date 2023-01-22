c60a95adf3bdd82d2c8903a2aa17334acef779a8
ayushpatnaikgit <ayushpatnaik@gmail.com>
2023-01-22 13:16:23 +0530

    Further speedup bootstrap algo by 2x.

--- src/total.jl
+++ src/total.jl
@@ -13,15 +13,15 @@ julia> total(:api00, clus_one_stage)
  Row │ total      SE
      │ Float64    Float64
 ─────┼──────────────────────
-   1 │ 3.98999e6  9.10443e5
+   1 │ 3.98999e6  9.01611e5
 
 julia> total([:api00, :enroll], clus_one_stage)
 2×3 DataFrame
  Row │ names   total      SE
      │ String  Float64    Float64
 ─────┼──────────────────────────────
-   1 │ api00   3.98999e6  9.10443e5
-   2 │ enroll  3.40494e6  9.47987e5
+   1 │ api00   3.98999e6  9.01611e5
+   2 │ enroll  3.40494e6  9.33396e5
 ```
 """
 function total(x::Symbol, design::ReplicateDesign)
@@ -52,17 +52,17 @@ julia> total(:api00, :cname, clus_one_stage)
  Row │ cname        total           SE
      │ String15     Float64         Any
 ─────┼────────────────────────────────────────
-   1 │ Santa Clara       6.44244e5  4.29558e5
-   2 │ San Diego         1.2276e6   8.60246e5
-   3 │ Merced        70300.2        70757.4
-   4 │ Los Angeles       3.2862e5   2.95688e5
-   5 │ Orange            3.84807e5  3.77128e5
-   6 │ Fresno        63903.1        64455.2
-   7 │ Plumas            2.16147e5  2.12279e5
-   8 │ Alameda      249080.0        2.5221e5
-   9 │ San Joaquin       6.90276e5  6.92353e5
-  10 │ Kern          30631.5        30333.5
-  11 │ Mendocino     84380.6        80774.4
+   1 │ Santa Clara       6.44244e5  4.2273e5
+   2 │ San Diego         1.2276e6   8.62727e5
+   3 │ Merced        70300.2        71336.3
+   4 │ Los Angeles       3.2862e5   2.93936e5
+   5 │ Orange            3.84807e5  3.88014e5
+   6 │ Fresno        63903.1        64781.7
+   7 │ Plumas            2.16147e5  2.12089e5
+   8 │ Alameda      249080.0        2.49228e5
+   9 │ San Joaquin       6.90276e5  6.81604e5
+  10 │ Kern          30631.5        30870.3
+  11 │ Mendocino     84380.6        80215.9
 ```
 """
 function total(x::Symbol, domain::Symbol, design::ReplicateDesign)
