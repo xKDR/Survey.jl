c49eb6c1f2d74ea2714dbcc4329dfd43f97dc3cd
ayushpatnaikgit <ayushpatnaik@gmail.com>
2023-01-31 15:26:27 +0530

    Adding tests for just point estimates and reorganising to prevent mistakes.

--- test/quantile.jl
+++ test/quantile.jl
@@ -1,13 +1,6 @@
 @testset "quantile_SimpleRandomSample" begin
-    ##### SimpleRandomSample tests
-    # Load API datasets
-    apisrs_original = load_data("apisrs")
-    apisrs_original[!, :derived_probs] = 1 ./ apisrs_original.pw
-    apisrs_original[!, :derived_sampsize] = fill(200.0, size(apisrs_original, 1))
-    ##############################
-    apisrs = copy(apisrs_original)
-    srs_design = SurveyDesign(apisrs; weights=:pw) |> bootweights
-    @test quantile(:api00, srs_design, 0.5)[!,1][1] ≈ 659.0 atol=1e-4
-    @test quantile(:api00, srs_design, [0.1753,0.25,0.5,0.75,0.975])[!,2] ≈ [512.8847,544,659,752.5,905] atol = 1e-4
-    @test quantile(:enroll,srs_design, [0.1,0.2,0.5,0.75,0.95])[!,2] ≈ [245.5,317.6,453.0,668.5,1473.1] atol = 1e-4 
+    @test quantile(:api00, srs, 0.5)[!,1][1] ≈ 659.0 atol=1e-4
+    @test quantile(:api00, srs_boot, 0.5)[!,1][1] ≈ 659.0 atol=1e-4
+    @test quantile(:api00, srs_boot, [0.1753,0.25,0.5,0.75,0.975])[!,2] ≈ [512.8847,544,659,752.5,905] atol = 1e-4
+    @test quantile(:enroll,srs_boot, [0.1,0.2,0.5,0.75,0.95])[!,2] ≈ [245.5,317.6,453.0,668.5,1473.1] atol = 1e-4 
 end
