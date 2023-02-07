42bb4331a83841fad761227338451c876a86a5a8
ayushpatnaikgit <ayushpatnaik@gmail.com>
2023-02-07 23:26:02 +0530

    Add se test

--- test/reg.jl
+++ test/reg.jl
@@ -2,4 +2,5 @@ using GLM
 @testset "GLM on simple random sample" begin
     repdesign = bootweights(srs; replicates = 500)
    @test glm(@formula(api00~api99), repdesign, Normal()).Coefficients ≈ coef(glm(@formula(api00~api99), repdesign.data, Normal()))
+   @test glm(@formula(api00~api99), repdesign, Normal()).SE ≈ stderror(glm(@formula(api00~api99), repdesign.data, Normal())) rtol = 1
 end
