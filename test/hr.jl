2a90d8a51533f202d841b19732fbc456b1f6808c
smishr <sm_data@outlook.com>
2023-02-08 14:25:25 +0530

    fix tests hartley

--- test/hr.jl
+++ test/hr.jl
@@ -1,11 +1,11 @@
 @testset "hartley_rao" begin
-    # base functionality
+    # base functionality SRS
     tot = total(:api00, srs)
-    @test tot.total[2] ≈ 4066888 rtol = STAT_TOL
-    
+    @test tot.SE[1] ≈ 57154.1 rtol = STAT_TOL
+    # Stratified
     tot = total(:api00, dstrat)
-    @test tot.total[2] ≈ 4066888 rtol = STAT_TOL
-
+    @test tot.SE[1] ≈ 699405 rtol = STAT_TOL
+    # one stage cluster
     tot = total(:api00, dclus1)
-    @test tot.total[2] ≈ 4066888 rtol = STAT_TOL
+    @test tot.SE[1] ≈ 4880240 rtol = STAT_TOL
 end
