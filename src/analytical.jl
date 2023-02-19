c73619908863eedf89de1d4e697bb44dcde2de93
smishr <sm_data@outlook.com>
2023-02-19 11:33:45 +0530

    Add some analytical solutions for deff calc

---
+++ src/analytical.jl
@@ -0,0 +1,26 @@
+"""
+    Variance under Simple Random Sampling scheme
+    see Lohr pg78.
+"""
+function srs_variance(x::Symbol, design::AbstractSurveyDesign)
+    ŝ = var(design.data[!,x]) ./ nrow(design.data)
+    X̂ = wsum(design.data[!, x], weights(design.data[!, design.weights]))
+    DataFrame(ht_total = X̂)
+end
+
+"""
+    Horvitz-Thompson total
+"""
+function ht_total(x::Symbol, design::AbstractSurveyDesign)
+    X̂ = wsum(design.data[!, x], weights(design.data[!, design.weights]))
+    DataFrame(ht_total = X̂)
+end
+
+"""
+    Strata totals
+"""
+function strata_totals(x::Symbol, design::AbstractSurveyDesign)
+    gdf_strata = groupby(design.data, design.strata)
+    X̂ₛ = combine(gdf_strata, [x, design.weights] => ((a, b) -> wsum(a, b)) => :total)
+    return X̂ₛ
+end
