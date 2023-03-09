bdbe00d87ee1ac3b8dd18a52ffe0607100425e9c
sayantikaSSG <sayantikasenguptassg@gmail.com>
2023-03-09 20:49:55 +0530

    srs and stratified with bootstrap

--- test/ratio.jl
+++ test/ratio.jl
@@ -3,4 +3,7 @@
     @test ratio(:api00, :enroll, dclus1_boot).SE[1] ≈ 0.1275446 atol = 1e-1
     @test ratio(:api00, :enroll, dclus1_boot).ratio[1] ≈ 1.17182 atol = 1e-4
     @test ratio(:api00, :enroll, srs).ratio[1] ≈ 1.1231 atol = 1e-4
+    @test ratio(:api99, :enroll, bsrs).ratio[1] ≈ 1.06854 atol = 1e-4
+    @test ratio(:api99, :enroll, dstrat).ratio[1] ≈ 1.0573 atol = 1e-4
+    @test ratio(:api99, :enroll, bstrat).ratio[1] ≈ 1.0573 atol = 1e-4
 end
