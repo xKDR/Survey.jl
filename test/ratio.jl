a9aca84349ca05a132027d6a644648ebc5bf65fc
sayantikaSSG <sayantikasenguptassg@gmail.com>
2023-03-09 20:13:05 +0530

    adding testcase to ratio

--- test/ratio.jl
+++ test/ratio.jl
@@ -2,4 +2,5 @@
     @test ratio(:api00, :enroll, dclus1).ratio[1] ≈ 1.17182 atol = 1e-4
     @test ratio(:api00, :enroll, dclus1_boot).SE[1] ≈ 0.1275446 atol = 1e-1
     @test ratio(:api00, :enroll, dclus1_boot).ratio[1] ≈ 1.17182 atol = 1e-4
+    @test ratio(:api00, :enroll, srs).ratio[1] ≈ 1.1231 atol = 1e-4
 end
