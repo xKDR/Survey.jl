a223253c77d1229b1c883c966d78f9f5951ecf69
smishr <sm_data@outlook.com>
2023-03-23 12:43:54 +0530

    improved formatting for srs strat tests

--- test/jackknife.jl
+++ test/jackknife.jl
@@ -5,8 +5,14 @@
     dclus2_jk = dclus2 |> jackknifeweights # Two stage cluster
     dnhanes_jk = dnhanes |> jackknifeweights # multistage stratified
     
-    mean(:api00,bsrs_jk).SE[1] ≈ 9.4028 atol = 1e-3 
-    mean([:api99,:api00],dstrat_jk).SE[1] ≈ 10.097  atol = 1e-3 
+    mean_srs_jk = mean([:api00,:api99], bsrs_jk)
+    @test mean_srs_jk.SE[1] ≈ 9.4028 atol = 1e-3
+    @test mean_srs_jk.SE[2] ≈ 9.6575 atol = 1e-3
+    
+    mean_strat_jk = mean([:api00,:api99],dstrat_jk)
+    @test mean_strat_jk.SE[1] ≈ 9.5361 atol = 1e-3
+    @test mean_strat_jk.SE[2] ≈ 10.097 atol = 1e-3
+    
     # mean(:api00,dclus2_jk)
     # mean(,dnhanes_jk)
 end
@@ -29,9 +35,10 @@ end
 # ## srs without fpc (matches Julia)
 # srs <- svydesign(id=~1,data=apisrs, weights=~pw)#, fpc=~fpc)
 # bsrs_jk <- as.svrepdesign(srs, type="JK1", compress=FALSE)
-# svymean(~api00,bsrs_jk)
+# svymean(~api00+api99,bsrs_jk)
 # #         mean     SE
 # # api00 656.58 9.4028
+# # api99 624.68 9.6575
 
 # # strat with fpc (doesnt match Julia)
 # dstrat <- svydesign(data=apistrat, id=~1, weights=~pw, strata=~stype,fpc=~fpc)
@@ -43,6 +50,7 @@ end
 # # strat without fpc (matches Julia)
 # dstrat <- svydesign(data=apistrat, id=~1, weights=~pw, strata=~stype)#,fpc=~fpc)
 # dstrat_jk <- as.svrepdesign(dstrat, type="JKn", compress=FALSE)
-# svymean(~api99,dstrat_jk)
+# svymean(~api00+api99,dstrat_jk)
 # #         mean     SE
-# # api99 629.39 10.097
+# # api00 662.29  9.5361
+# # api99 629.39 10.0970
