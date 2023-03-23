0985ec1077fb274ef4f2f2ff5f7f58b08549a375
smishr <sm_data@outlook.com>
2023-03-23 12:34:03 +0530

    added JK tests srs, strat

--- test/jackknife.jl
+++ test/jackknife.jl
@@ -1,7 +1,48 @@
 @testset "jackknife" begin
-    dstrat_jk = jackknifeweights(dstrat)
-    mean(:api00,dstrat_jk)
-    mean(:api99,dstrat_jk)
-    # dnhanes_jk = jackknifeweights(dnhanes)
-    # dclus1_jk = jackknifeweights(dclus1)
+    # Create Jackknife replicate designs
+    bsrs_jk = srs |> jackknifeweights # SRS
+    dstrat_jk = dstrat |> jackknifeweights # Stratified
+    dclus2_jk = dclus2 |> jackknifeweights # Two stage cluster
+    dnhanes_jk = dnhanes |> jackknifeweights # multistage stratified
+    
+    mean(:api00,bsrs_jk).SE[1] ≈ 9.4028 atol = 1e-3 
+    mean([:api99,:api00],dstrat_jk).SE[1] ≈ 10.097  atol = 1e-3 
+    # mean(:api00,dclus2_jk)
+    # mean(,dnhanes_jk)
 end
+
+# # R code for correctness above
+# library(survey) 
+# data(api)
+# apiclus1$pw = rep(757/15,nrow(apiclus1))
+
+# #############
+# ###### 23.03.22 PR#260
+
+# ## srs with fpc (doesnt match Julia)
+# srs <- svydesign(id=~1,data=apisrs, weights=~pw, fpc=~fpc)
+# bsrs_jk <- as.svrepdesign(srs, type="JK1", compress=FALSE)
+# svymean(~api00,bsrs_jk)
+# #         mean     SE
+# # api00 656.58 9.2497
+
+# ## srs without fpc (matches Julia)
+# srs <- svydesign(id=~1,data=apisrs, weights=~pw)#, fpc=~fpc)
+# bsrs_jk <- as.svrepdesign(srs, type="JK1", compress=FALSE)
+# svymean(~api00,bsrs_jk)
+# #         mean     SE
+# # api00 656.58 9.4028
+
+# # strat with fpc (doesnt match Julia)
+# dstrat <- svydesign(data=apistrat, id=~1, weights=~pw, strata=~stype,fpc=~fpc)
+# dstrat_jk <- as.svrepdesign(dstrat, type="JKn", compress=FALSE)
+# svymean(~api99,dstrat_jk)
+# #         mean     SE
+# # api99 629.39 9.9639
+
+# # strat without fpc (matches Julia)
+# dstrat <- svydesign(data=apistrat, id=~1, weights=~pw, strata=~stype)#,fpc=~fpc)
+# dstrat_jk <- as.svrepdesign(dstrat, type="JKn", compress=FALSE)
+# svymean(~api99,dstrat_jk)
+# #         mean     SE
+# # api99 629.39 10.097
