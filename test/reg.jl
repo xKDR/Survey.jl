40bb2252fd4e9a75bddd4839367c12e30128bff8
nadiaenh <nadia.enhaili@gmail.com>
2023-06-11 03:08:09 -0700

    lower replicates in reg.jl test for faster runtime

--- test/reg.jl
+++ test/reg.jl
@@ -1,7 +1,9 @@
-@testset "Normal GLM on bootstrap simple random sample survey" begin
-    glm_model = glm(@formula(api00 ~ api99), bsrs.data, Normal())
-    svyglm_result = svyglm(@formula(api00 ~ api99), bsrs, Normal())
+@testset "Gamma GLM on bsrs" begin
+    bsrs = bootweights(srs; replicates=1000)
+    glm_model = glm(@formula(api00 ~ api99), bsrs.data, Gamma())
+    svyglm_result = svyglm(@formula(api00 ~ api99), bsrs, Gamma())
 
     @test svyglm_result.Coefficients ≈ coef(glm_model) rtol=STAT_TOL
-    @test svyglm_result.SE ≈ stderror(glm_model) rtol=SE_TOL
+    @test svyglm_result.SE[0] ≈ 9.77056319 rtol=SE_TOL
+    @test svyglm_result.SE[1] ≈ 0.01397871 rtol=SE_TOL
 end
