0aed9907cde777e903d30eeb44938d3ad64cce29
ayushpatnaikgit <ayushpatnaik@gmail.com>
2023-08-04 13:27:25 +1000

    Code reuse in bydomain for SurveyDesign

--- src/by.jl
+++ src/by.jl
@@ -1,7 +1,11 @@
-function bydomain(x::Symbol, domain, design::SurveyDesign, func::Function)
+function bydomain(x::Union{Symbol, Vector{Symbol}}, domain,design::SurveyDesign, func::Function, args...; kwargs...)
     gdf = groupby(design.data, domain)
-    X = combine(gdf, [x, design.weights] => ((a, b) -> func(a, weights(b))) => :statistic)
-    return X
+    vars = DataFrame[]
+    for group in gdf
+        rep_domain = SurveyDesign(DataFrame(group);clusters = design.cluster, strata = design.strata, popsize = design.popsize, weights = design.weights)   
+        push!(vars, func(x, rep_domain, args...; kwargs...))
+    end
+    return vcat(vars...)
 end
 
 function bydomain(x::Union{Symbol, Vector{Symbol}}, domain,design::ReplicateDesign{BootstrapReplicates}, func::Function, args...; kwargs...)
@@ -9,7 +13,7 @@ function bydomain(x::Union{Symbol, Vector{Symbol}}, domain,design::ReplicateDesi
     vars = DataFrame[]
     for group in gdf
         rep_domain = ReplicateDesign{typeof(design.inference_method)}(DataFrame(group), design.replicate_weights;clusters = design.cluster, strata = design.strata, popsize = design.popsize, weights = design.weights)   
-        push!(vars, func(x, rep_domain))
+        push!(vars, func(x, rep_domain, args...; kwargs...))
     end
     return vcat(vars...)
 end
