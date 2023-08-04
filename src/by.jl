42130f5008b8d4c41931c10c25f6a98a34d0957d
ayushpatnaikgit <ayushpatnaik@gmail.com>
2023-08-04 13:18:59 +1000

    Remove comment

--- src/by.jl
+++ src/by.jl
@@ -10,7 +10,6 @@ function bydomain(x::Union{Symbol, Vector{Symbol}}, domain,design::ReplicateDesi
     for group in gdf
         rep_domain = ReplicateDesign{typeof(design.inference_method)}(DataFrame(group), design.replicate_weights;clusters = design.cluster, strata = design.strata, popsize = design.popsize, weights = design.weights)   
         push!(vars, func(x, rep_domain))
-        # push!(vars, variance(x, func, rep_domain , args...; kwargs...))
     end
     return vcat(vars...)
 end
