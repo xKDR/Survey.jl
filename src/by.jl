54f822038d775f2a9f95622d238da557c2a1104e
nadiaenh <nadia.enhaili@gmail.com>
2023-08-09 04:16:49 +0000

    remove jackknife replicate support in bydomain

--- src/by.jl
+++ src/by.jl
@@ -2,11 +2,11 @@ function subset(group, design::SurveyDesign)
     return SurveyDesign(DataFrame(group);clusters = design.cluster, strata = design.strata, popsize = design.popsize, weights = design.weights)   
 end
 
-function subset(group, design::ReplicateDesign)
+function subset(group, design::ReplicateDesign{BootstrapReplicates})
     return ReplicateDesign{typeof(design.inference_method)}(DataFrame(group), design.replicate_weights;clusters = design.cluster, strata = design.strata, popsize = design.popsize, weights = design.weights)   
 end
 
-function bydomain(x::Union{Symbol, Vector{Symbol}}, domain,design::AbstractSurveyDesign, func::Function, args...; kwargs...)
+function bydomain(x::Union{Symbol, Vector{Symbol}}, domain,design::Union{SurveyDesign, ReplicateDesign{BootstrapReplicates}}, func::Function, args...; kwargs...)
     gdf = groupby(design.data, domain)
     vars = DataFrame[]
     for group in gdf
