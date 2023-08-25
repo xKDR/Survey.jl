b0015c11e0249069408f9d5e6dc754d0d057819a
nadiaenh <nadia.enhaili@gmail.com>
2023-08-25 04:28:03 +0000

    update svyglm to use new variance func

--- src/reg.jl
+++ src/reg.jl
@@ -22,18 +22,15 @@ result = svyglm(@formula(api00 ~ api99), bsrs, Normal())
 """
 
 function svyglm(formula::FormulaTerm, design::ReplicateDesign, args...; kwargs...)
-    # Compute estimates for model coefficients
-    model = glm(formula, design.data, args...; wts = design.data[!, design.weights], kwargs...)
-    main_coefs = coef(model)
-  
-    # Compute replicate models and coefficients
-    n_replicates = parse(Int, string(design.replicates))
-    rep_models = [glm(formula, design.data, args...; wts = design.data[!, "replicate_"*string(i)]) for i in 1:n_replicates]
-    rep_coefs = [coef(model) for model in rep_models] # vector of vectors [n_replicates x [n_coefs]]
-    rep_coefs = hcat(rep_coefs...) # matrix of floats [n_coefs x n_replicates]
-    n_coefs = size(rep_coefs)[1]
-  
-    # Compute standard errors of coefficients
-    SE = [std(rep_coefs[i,:]) for i in 1:n_coefs]
-    DataFrame(Coefficients = main_coefs, SE = SE)
+
+    columns = vcat(collect(Symbol.(formula.rhs)), Symbol.(formula.lhs))
+
+    function inner_svyglm(df::DataFrame, columns, weights_column, args...; kwargs...)
+        matrix = hcat(ones(nrow(df)), Matrix(df[!, columns[1:(length(columns)-1)]]))
+        model = glm(matrix, (df[!, columns[end]]), args...; kwargs...)
+        return coef(model)
+    end
+
+    # Compute variance of coefficients
+    variance(columns, inner_svyglm, design, args...; kwargs...)
   end
--- test/reg.jl
+++ test/reg.jl
@@ -1,24 +1,31 @@
 @testset "Binomial GLM in bsrs" begin
-    bsrs = bootweights(srs, replicates=500)
+    bsrs = bootweights(srs, replicates=10000)
     rename!(bsrs.data, Symbol("sch.wide") => :sch_wide)
     bsrs.data.sch_wide = ifelse.(bsrs.data.sch_wide .== "Yes", 1, 0)
     model = svyglm(@formula(sch_wide ~ meals + ell), bsrs, Binomial())
 
-    @test model.Coefficients[1] ≈ 1.523050676 rtol=STAT_TOL
-    @test model.Coefficients[2] ≈ 0.009754261 rtol=STAT_TOL
-    @test model.Coefficients[3] ≈ -0.020892044 rtol=STAT_TOL
+    @test model.estimator[1] ≈ 1.523050676 rtol=STAT_TOL
+    @test model.estimator[2] ≈ 0.009754261 rtol=STAT_TOL
+    @test model.estimator[3] ≈ -0.020892044 rtol=STAT_TOL
 
     @test model.SE[1] ≈ 0.368055384 rtol=SE_TOL
     @test model.SE[2] ≈ 0.009798694 rtol=SE_TOL
     @test model.SE[3] ≈ 0.012806313 rtol=SE_TOL
+
+    # R code
+    # data(api)
+    # srs <- svydesign(id=~dnum, weights=~pw, data=apisrs)
+    # bsrs <- as.svrepdesign(srs, type="subbootstrap", replicates=10000)
+    # model = svyglm(sch.wide ~ meals + ell, bsrs, family=binomial())
+    # summary(model)
 end
 
 @testset "Gamma GLM in bsrs" begin
     bsrs = bootweights(srs, replicates=500)
     model = svyglm(@formula(api00 ~ api99), bsrs, Gamma())
 
-    @test model.Coefficients[1] ≈ 2.915479e-03 rtol=STAT_TOL
-    @test model.Coefficients[2] ≈ -2.137187e-06 rtol=STAT_TOL
+    @test model.estimator[1] ≈ 2.915479e-03 rtol=STAT_TOL
+    @test model.estimator[2] ≈ -2.137187e-06 rtol=STAT_TOL
     @test model.SE[1] ≈ 4.581166e-05 rtol=SE_TOL
     @test model.SE[2] ≈ 6.520643e-08 rtol=SE_TOL
 end
@@ -27,8 +34,8 @@ end
     bsrs = bootweights(srs, replicates=500)
     model = svyglm(@formula(api00 ~ api99), bsrs, Normal())
 
-    @test model.Coefficients[1] ≈ 63.2830726 rtol=STAT_TOL
-    @test model.Coefficients[2] ≈ 0.9497618 rtol=STAT_TOL
+    @test model.estimator[1] ≈ 63.2830726 rtol=STAT_TOL
+    @test model.estimator[2] ≈ 0.9497618 rtol=STAT_TOL
     @test model.SE[1] ≈ 9.64853456 rtol=SE_TOL
     @test model.SE[2] ≈ 0.01378683 rtol=SE_TOL
 end
@@ -38,12 +45,11 @@ end
     rename!(bsrs.data, Symbol("api.stu") => :api_stu)
     model = svyglm(@formula(api_stu ~ meals + ell), bsrs, Poisson())
 
-    @test model.Coefficients[1] ≈ 6.229602881 rtol=STAT_TOL
-    @test model.Coefficients[2] ≈ -0.002038345 rtol=STAT_TOL
-    @test model.Coefficients[3] ≈ 0.002116465 rtol=STAT_TOL
+    @test model.estimator[1] ≈ 6.229602881 rtol=STAT_TOL
+    @test model.estimator[2] ≈ -0.002038345 rtol=STAT_TOL
+    @test model.estimator[3] ≈ 0.002116465 rtol=STAT_TOL
 
     @test model.SE[1] ≈ 0.093944656 rtol=SE_TOL
     @test model.SE[2] ≈ 0.002176296 rtol=SE_TOL
     @test model.SE[3] ≈ 0.002841031 rtol=SE_TOL
 end
-
